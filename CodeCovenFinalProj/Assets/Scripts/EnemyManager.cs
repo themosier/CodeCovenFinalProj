33b3e735cff11a8d13d88d427f1509b48d8858f7
Devon Mosier <themosier11@gmail.com>
2021-04-14 23:02:44 -0700

    Added some test scripts for moving enemies toward Player object

---
+++ CodeCovenFinalProj/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Object enemyPrefab;
+    public static GameObject player;
+
+    public static int maxEnemies = 5;
+    public static int curEnemies;
+    private bool isSpawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+        curEnemies = 0;
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSpawning && curEnemies < maxEnemies)
+        {
+            //Invoke("SpawnEnemy", 1.0f);
+            isSpawning = true;
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        Vector3 pos = GetComponent<Transform>().position;
+        Instantiate(enemyPrefab, pos, Quaternion.identity);
+        curEnemies++;
+        isSpawning = false;
+    }
+}
---
+++ CodeCovenFinalProj/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 forward;
+
+    //public GameObject player;
+
+    public float velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        forward = rb.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 vec = rb.position - player.transform.position;
+        //Vector3 vec = player.transform.position + rb.position;
+        //rb.velocity = -vec * velocity;
+
+        Vector3 vec = new Vector3(EnemyManager.player.transform.position.x, transform.position.y, EnemyManager.player.transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, vec, velocity * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            Destroy(gameObject);
+            EnemyManager.curEnemies--;
+        }
+    }
+}
