9847cf5c2cd56749edffcbf0c8a28915e76d9a79
michisumi <michisumiart@gmail.com>
2021-04-21 01:03:48 -0400

    HUD Health Bar
    Added health bar icon and functionality
    Refer to PlayerManager script to tweak damage

--- CodeCovenFinalProj/Assets/Scripts/PlayerManager.cs
+++ CodeCovenFinalProj/Assets/Scripts/PlayerManager.cs
@@ -11,12 +11,21 @@ public class PlayerManager : MonoBehaviour
 
     public static GameObject player;
 
+    //Player Health System
+    public int maxHealth = 50;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
 
         Cursor.lockState = CursorLockMode.Locked;
+
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -25,6 +34,19 @@ public class PlayerManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("-5 Damage");
+            TakeDamage(5);
+        }
+
+        void TakeDamage (int damage)
+        {
+            currentHealth -= damage;
+
+            healthBar.SetHealth(currentHealth);
         }
     }
 }
